fd1036cc2a60ec81ab770cfc388fc10d4d84ecbb
Aakash Ahuja <aakashahuja30495@gmail.com>
2020-10-14 16:49:21 -0400

    Updates

---
+++ precache-manifest.6d5a6bc5baa25fe89621da7aa6077612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f923a0ad82e67e8fdbcde18ea3e8c0",
+    "url": "/HandposeDetection/index.html"
+  },
+  {
+    "revision": "f81623a7dc5af7f7b0c1",
+    "url": "/HandposeDetection/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "84f57646510e4c0c33a2",
+    "url": "/HandposeDetection/static/js/2.c479ab58.chunk.js"
+  },
+  {
+    "revision": "6a1f054199e62a32efd082e739a490d7",
+    "url": "/HandposeDetection/static/js/2.c479ab58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f81623a7dc5af7f7b0c1",
+    "url": "/HandposeDetection/static/js/main.2d00cb4b.chunk.js"
+  },
+  {
+    "revision": "732afafb97913f52ec43",
+    "url": "/HandposeDetection/static/js/runtime-main.b1317198.js"
+  }
+]);
